af9cd9674a04a7b7f9029482ffe727a50df3e814
Sulthan Ahmed <sultanahmeduk@gmail.com>
2015-12-16 18:01:52 +0000

    - added express - added handlers for 500 and 404s - added a listener - started to try to use hogan express

---
+++ server.js
@@ -0,0 +1,27 @@
+//imports
+var express = require('express');
+
+var app = express();
+
+//use the provided PORT or 4000
+app.set('port', process.env.PORT || 4000);
+
+// set up view engine
+app.engine('html', require('hogan-express'));
+app.set('views engine', 'html');
+
+//Express adds middleware when you use app.use
+//catch 404 issues
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(404);
+});
+
+app.use(function (err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+});
